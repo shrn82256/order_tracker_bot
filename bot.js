96f3c34914d35a2f28a82e63016dac6a87078c89
shrn82256 <shrn82256@gmail.com>
2019-04-16 18:47:59 +0530

    attempt to send photo

--- bot.js
+++ bot.js
@@ -1,5 +1,58 @@
 const token = process.env.TELEGRAM_API_TOKEN;
 
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+global.currentChatId = -1;
+global.currentBody = "";
+global.count = 0;
+/* 
+var intervalID = setInterval(() => {
+  console.log("Hello World!");
+  (async ({ EMAIL, PASSWORD, REDIRECT }) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.cubelelo.com/login");
+
+    await page.waitFor(`input[name="email"]`);
+    await page.$eval(
+      `input[name="email"]`,
+      (el, value) => (el.value = value),
+      EMAIL
+    );
+    await page.waitFor(`input[name="password"]`);
+    await page.$eval(
+      `input[name="password"]`,
+      (el, value) => (el.value = value),
+      PASSWORD
+    );
+
+    await page.click(`input[value="Login"]`);
+    await page.waitForNavigation();
+
+    await page.goto(REDIRECT);
+
+    const containerText = await page.evaluate(
+      () => document.getElementById("group-content").textContent
+    );
+
+    // console.log(containerText);
+
+    if (global.currentBody != containerText) {
+      global.currentBody = containerText;
+      global.count += 1;
+      await page.screenshot({ path: "example.png", fullPage: true });
+    }
+
+    console.log("global count", global.count);
+    console.log("New Page URL:", page.url());
+
+    await browser.close();
+
+    return containerText;
+  })(process.env);
+}, 1000 * 20);
+ */
 const Bot = require("node-telegram-bot-api");
 let bot;
 
@@ -17,7 +70,7 @@ bot.on("message", msg => {
   const name = msg.from.first_name;
   const text = msg.text;
   bot.sendMessage(msg.chat.id, `Hello ${name}! ${text}`).then(() => {
-    // reply sent!
+    bot.sendPhoto(msg.chat.id, __dirname + "/sample.jpg");
   });
 });
 
