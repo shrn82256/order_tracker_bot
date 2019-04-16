921838fd547bec8948017410220f09de7f92682f
shrn82256 <shrn82256@gmail.com>
2019-04-16 18:25:44 +0530

    return message

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ console.log("Bot server started in the " + process.env.NODE_ENV + " mode");
 
 bot.on("message", msg => {
   const name = msg.from.first_name;
-  console.log(msg);
-  bot.sendMessage(msg.chat.id, "Hello, " + name + "!").then(() => {
+  const text = msg.text;
+  bot.sendMessage(msg.chat.id, `Hello ${name}! ${text}`).then(() => {
     // reply sent!
   });
 });
