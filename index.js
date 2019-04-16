301e126decb6637148b0a2eafe6c20fabb6c5e9a
shrn82256 <shrn82256@gmail.com>
2019-04-16 17:39:56 +0530

    trying echo-bot sample

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 var telegram = require("telegram-bot-api");
-
-require("dotenv").config();
-
 var api = new telegram({
-  token: process.env.TELEGRAM_API_TOKEN
+  token: process.env.TELEGRAM_API_TOKEN,
+  updates: {
+    enabled: true,
+    get_interval: 1000
+  }
 });
 
-api
-  .getMe()
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+api.on("message", function(message) {
+  var chat_id = message.chat.id;
+
+  // It'd be good to check received message type here
+  // And react accordingly
+  // We consider that only text messages can be received here
+
+  api
+    .sendMessage({
+      chat_id: message.chat.id,
+      text: message.text ? message.text : "This message doesn't contain text :("
+    })
+    .then(function(message) {
+      console.log(message);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
