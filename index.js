5663d4848840b96064f66ca7ca0caf4ba8b39424
shrn82256 <shrn82256@gmail.com>
2019-04-16 17:30:48 +0530

    trying out telegram-bot-api from npm

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-const axios = require("axios");
+var telegram = require("telegram-bot-api");
 
 require("dotenv").config();
 
-var app = express();
-
-app.use(bodyParser.json());
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-let telegram_url =
-  "https://api.telegram.org/bot" +
-  process.env.TELEGRAM_API_TOKEN +
-  "/sendMessage";
-
-function sendMessage(url, message, reply, res) {
-  axios
-    .post(url, { chat_id: message.chat.id, text: reply })
-    .then(response => {
-      console.log("Message posted");
-      res.end("ok");
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-app.post("/start_bot", function(req, res) {
-  const { message } = req.body;
-  if ("text" in message) {
-    var reply = `${message.text} to you too`;
-  } else {
-    var reply = "kuch dhang ka likh be";
-    console.log(message);
-  }
-
-  sendMessage(telegram_url, message, reply, res);
+var api = new telegram({
+  token: process.env.TELEGRAM_API_TOKEN
 });
 
-app.get("/test", (req, res) => res.send("Hello World!"));
-
-app.listen(process.env.PORT, () =>
-  console.log("Telegram bot is listening on port 3000!")
-);
+api
+  .getMe()
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
--- scrapper.js
+++ scrapper.js
@@ -1,22 +1,58 @@
-const https = require("https");
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
-const options = {
-  hostname: "https://www.cubelelo.com/login",
-  port: 443,
-  path: "/",
-  method: "GET"
-};
+global.currentBody = "";
+global.count = 0;
 
-const req = https.request(options, res => {
-  console.log("statusCode:", res.statusCode);
-  console.log("headers:", res.headers);
+var intervalID = setInterval(() => {
+  console.log("Hello World!");
+  (async ({ EMAIL, PASSWORD, REDIRECT }) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.cubelelo.com/login");
 
-  res.on("data", d => {
-    process.stdout.write(d);
-  });
-});
+    await page.waitFor(`input[name="email"]`);
+    await page.$eval(
+      `input[name="email"]`,
+      (el, value) => (el.value = value),
+      EMAIL
+    );
+    await page.waitFor(`input[name="password"]`);
+    await page.$eval(
+      `input[name="password"]`,
+      (el, value) => (el.value = value),
+      PASSWORD
+    );
+    /* await page.waitFor(`input[name="redirect"]`);
+    await page.$eval(
+      `input[name="redirect"]`,
+      (el, value) => (el.value = value),
+      REDIRECT
+    ); */
 
-req.on("error", e => {
-  console.error(e);
-});
-req.end();
+    await page.click(`input[value="Login"]`);
+    await page.waitForNavigation();
+
+    await page.goto(REDIRECT);
+
+    const containerText = await page.evaluate(
+      () => document.getElementById("group-content").textContent
+    );
+
+    // console.log(containerText);
+
+    if (global.currentBody !== containerText)
+      console.log("global count", global.count);
+
+    console.log("New Page URL:", page.url());
+
+    global.currentBody = containerText;
+    global.count += 1;
+
+    await page.screenshot({ path: "example.png", fullPage: true });
+
+    await browser.close();
+
+    return containerText;
+  })(process.env);
+}, 1000 * 20);
